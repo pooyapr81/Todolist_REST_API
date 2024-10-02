38b3859a2e05df0a1569cb6c7a25b98faf61227e
pooyapr81 <Pooya.parva81@gmail.com>
2024-10-02 15:53:43 +0330

    complite GET method

--- task/api/urls.py
+++ task/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('tasks/', views.task_list, name='task_list'),
+    path('task/<int:id>', views.api_task_detail, name='task-detail'),
 ]
--- task/api/views.py
+++ task/api/views.py
@@ -5,10 +5,20 @@ from ..models import TodoTask
 from .serializers import TodoTaskSerializer
 
 
-
 @api_view(['GET'])
 def task_list(request):
     if request.method == "GET":
         query = TodoTask.objects.all()
         serializer = TodoTaskSerializer(query, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def api_task_detail(request, id):
+    try:
+        task = TodoTask.objects.get(pk=id)
+    except TodoTask.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializer = TodoTaskSerializer(task)
         return Response(serializer.data)
