4d170396773df3bc90058ef5ec01b951a7fe203c
pooyapr81 <Pooya.parva81@gmail.com>
2024-10-02 16:07:51 +0330

    write POST,DELETE and PUT method

--- task/api/urls.py
+++ task/api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('tasks/', views.task_list, name='task_list'),
     path('task/<int:id>', views.api_task_detail, name='task-detail'),
+    path('task/<int:pk>/update', views.api_task_update, name="task-update"),
+    path('task/<int:pk>/delete', views.api_task_delete, name="task-delete"),
+    path('task/create', views.api_task_create, name='task-create')
 ]
--- task/api/views.py
+++ task/api/views.py
@@ -22,3 +22,45 @@ def api_task_detail(request, id):
     if request.method == "GET":
         serializer = TodoTaskSerializer(task)
         return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def api_task_update(request, pk):
+    try:
+        task = TodoTask.objects.get(pk=pk)
+    except TodoTask.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = {}
+    if request.method == 'PUT':
+        serializer = TodoTaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data['anjam shod'] = "update anjam shod"
+            return Response(data=data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def api_task_delete(request, pk):
+    try:
+        task = TodoTask.objects.get(pk=pk)
+    except TodoTask.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    data = {}
+    if request.method == "DELETE":
+        task.delete()
+        data['hazf shod'] = "hazf anjam shod"
+        return Response(data=data)
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["POST"])
+def api_task_create(request):
+    data = {}
+    if request.method == "POST":
+        serializer = TodoTaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data['anjam shod'] = "task jadidi sakhte shod"
+            return Response(data=data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
